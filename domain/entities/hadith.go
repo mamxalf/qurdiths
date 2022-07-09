a0dcf7a8aa7719cc867b2baf310064c583e0d56e
Hammam Firdaus <hammam.firdaus@mekari.com>
2022-07-10 05:27:39 +0700

    add entity for json parse and build hadith

--- domain/entities/hadith.go
+++ domain/entities/hadith.go
@@ -5,3 +5,16 @@ type ListBooks struct {
 	Name   string
 	Amount int32
 }
+
+type HadithJson struct {
+	Number     int32  `json:"number"`
+	Arabian    string `json:"arab"`
+	Indonesian string `json:"id"`
+}
+
+type Hadith struct {
+	Book       string
+	Number     int32
+	Arabian    string
+	Indonesian string
+}
