2ca3871aed526b4ec2921ce88681ce5d923d140b
Hammam Firdaus <hammam.firdaus@mekari.com>
2022-07-09 21:35:18 +0700

    create fiber config

---
+++ app/config/fiber.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"hadithgo/app/exceptions"
+)
+
+func NewFiberConfig() fiber.Config {
+	return fiber.Config{
+		ErrorHandler: exceptions.ErrorHandler,
+	}
+}
+
+func CorsConfig() cors.Config {
+	return cors.Config{
+		AllowOrigins: "*",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}
+}
+
+func LoggerConfig() logger.Config {
+	return logger.Config{
+		Format:   "[${time}] - ${method} ${path} ~ [${status}][${latency}]\n",
+		TimeZone: "Local",
+	}
+}
