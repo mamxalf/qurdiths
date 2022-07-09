bb38ef18d74dca1394298885eceb2d1f41260da5
Hammam Firdaus <hammam.firdaus@mekari.com>
2022-07-09 23:32:02 +0700

    rename model -> models

--- app/exceptions/error_handler.go
+++ app/exceptions/error_handler.go
@@ -2,21 +2,21 @@ package exceptions
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"hadithgo/domain/model"
+	"hadithgo/domain/models"
 )
 
 func ErrorHandler(ctx *fiber.Ctx, err error) error {
 
 	_, ok := err.(ValidationError)
 	if ok {
-		return ctx.JSON(model.WebResponse{
+		return ctx.JSON(models.WebResponse{
 			Code:   400,
 			Status: "BAD_REQUEST",
 			Data:   err.Error(),
 		})
 	}
 
-	return ctx.JSON(model.WebResponse{
+	return ctx.JSON(models.WebResponse{
 		Code:   500,
 		Status: "INTERNAL_SERVER_ERROR",
 		Data:   err.Error(),
--- domain/models/web_response.go
+++ domain/models/web_response.go
@@ -1,4 +1,4 @@
-package model
+package models
 
 type WebResponse struct {
 	Code   int         `json:"code"`
